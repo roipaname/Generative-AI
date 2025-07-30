7edec840bb5c477a7c0530d50df654b1d7c0c520
roipaname <roipaname@gmail.com>
2025-07-31 00:15:26 +0200

    DATA

--- training/main.py
+++ training/main.py
@@ -1,1 +1,7 @@
 """ test 1"""
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("harrywang/acquired-podcast-transcripts-and-rag-evaluation")
+
+print("Path to dataset files:", path)
