a47cb0c91281291600a468724bb1cbf0a4f4ead9
roipaname <roipaname@gmail.com>
2025-08-01 13:07:08 +0200

    abg

--- tpu_test.py
+++ tpu_test.py
@@ -1,13 +1,3 @@
-import torch
-import torch_xla
 import torch_xla.core.xla_model as xm
-
-# Create a tensor
-device = xm.xla_device()
-t1 = torch.ones(2, 2, device=device)
-t2 = torch.ones(2, 2, device=device)
-print("TPU Device:", device)
-
-# Add tensors
-t3 = t1 + t2
-print("Result:", t3.cpu())
+print(xm.get_xla_supported_devices())
+)
