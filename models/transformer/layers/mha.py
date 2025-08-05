c4a7932a90a77c798ac51aba756b63e5dd628d44
roipaname <roipaname@gmail.com>
2025-08-05 08:34:15 +0200

    latest

--- models/transformer/layers/mha.py
+++ models/transformer/layers/mha.py
@@ -36,3 +36,51 @@ class MultiHeadAttention(nn.Module):
         out = attn @ v  # (B, H, T, D)
         out = out.transpose(1, 2).contiguous().view(B, T, C)
         return self.out_proj(out)
+import tiktoken
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset,DataLoader
+class GPTDatatsetV1(Dataset):
+  def __init__(self,txt,tokenizer,max_length,stride) :
+    self.input_ids=[]
+    self.target_ids=[]
+    token_ids=tokenizer.encode(txt,allowed_special={'<|endoftext|>'})
+    for i in range(0,len(token_ids)-max_length,stride):
+      self.input_chunk=token_ids[i:i+max_length]
+      self.target_chunk= token_ids[i+1:i+max_length+1]
+      self.input_ids.append(torch.tensor(self.input_chunk))
+      self.target_ids.append(torch.tensor(self.target_chunk))
+  def __len__(self):
+    return len(self.input_ids)
+  def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+def create_dataloader(txt, batch_size=4, max_length=256, stride=128, shuffle=True):
+  tokenizer=tiktoken.get_encoding("gpt2")
+  dataset=GPTDatatsetV1(txt,tokenizer,max_length,stride)
+  dataloader=DataLoader(
+      dataset,
+      batch_size=batch_size,
+      shuffle=shuffle,
+      drop_last=True
+  )
+  return dataloader
+
+with open("small-text-sample.txt",'r',encoding="utf-8") as f:
+  raw_text=f.read()
+tokenizer = tiktoken.get_encoding("gpt2")
+encoded_text = tokenizer.encode(raw_text)
+
+vocab_size = 50257
+output_dim = 256
+max_len = 1024
+context_length = max_len
+
+
+token_embedding_layer = nn.Embedding(vocab_size, output_dim)
+pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+
+max_length = 4
+dataloader = create_dataloader(raw_text, batch_size=8, max_length=max_length, stride=max_length)
+
+
+
